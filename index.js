25e5adf1399c8923cae96c17ca0c9276163cc002
anaxios <skybosh.xyz@outlook.com>
2024-07-21 20:08:30 -0700

    fix db location

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
 /* eslint radix: ["error", "as-needed"] */
-'use strict';
-const express = require('express');
-const fs = require('fs');
-const julianDate = require('./Classes/JulianDate');
+"use strict";
+const express = require("express");
+const fs = require("fs");
+const julianDate = require("./Classes/JulianDate");
 // Const Route = require('./Classes/Route');
 
 const app = express();
 
-const calendarDatabase = JSON.parse(fs.readFileSync('calendar_database.json', 'utf8'));
+const calendarDatabase = JSON.parse(
+  fs.readFileSync("./db/calendar_database.json", "utf8")
+);
 
-app.get('/api/calendar', (req, res) => {
-	const today = new Date();
-	const [month, date] = [today.getMonth(), today.getDate()];
+app.get("/api/calendar", (req, res) => {
+  const today = new Date();
+  const [month, date] = [today.getMonth(), today.getDate()];
 
-	let selection;
-	if (req.query.NewCalendar === 'true') {
-		selection = calendarDatabase.filter(
-			e => parseInt(e.month) === month + 1 && parseInt(e.date) === date,
-		);
-	} else {
-		const julianToday = julianDate.toJulian(today); // Hacky way to get old calendar current date.
-		console.log(julianToday);
-		selection = calendarDatabase.filter(
-			e =>
-				parseInt(e.month) === julianToday.getMonth() + 1
-        && parseInt(e.date) === julianToday.getDate(),
-		);
-	}
+  let selection;
+  if (req.query.NewCalendar === "true") {
+    selection = calendarDatabase.filter(
+      (e) => parseInt(e.month) === month + 1 && parseInt(e.date) === date
+    );
+  } else {
+    const julianToday = julianDate.toJulian(today); // Hacky way to get old calendar current date.
+    console.log(julianToday);
+    selection = calendarDatabase.filter(
+      (e) =>
+        parseInt(e.month) === julianToday.getMonth() + 1 &&
+        parseInt(e.date) === julianToday.getDate()
+    );
+  }
 
-	if (!selection || selection.length === 0) {
-		return res.status(404).send('Date not found.');
-	}
+  if (!selection || selection.length === 0) {
+    return res.status(404).send("Date not found.");
+  }
 
-	res.send(selection);
+  res.send(selection);
 });
 
-app.get('/api/calendar/:month/:date', (req, res) => {
-	const selection = calendarDatabase.filter(
-		e => e.month === req.params.month && e.date === req.params.date,
-	);
+app.get("/api/calendar/:month/:date", (req, res) => {
+  const selection = calendarDatabase.filter(
+    (e) => e.month === req.params.month && e.date === req.params.date
+  );
 
-	if (!selection || selection.length === 0) {
-		return res.status(404).send('Date not found.');
-	}
+  if (!selection || selection.length === 0) {
+    return res.status(404).send("Date not found.");
+  }
 
-	res.send(selection);
+  res.send(selection);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`listening on ${port}`);
+  console.log(`listening on ${port}`);
 });
